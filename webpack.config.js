da4cb8db3e169f0abfa895d99d87d71e8023a3c4
ianzyon <iansalgado@yahoo.com.br>
2019-11-02 15:36:56 -0300

    setup production webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,78 @@
 // where? entry --> output bundle.js
 const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    entry: './src/app.js',
-    // bundle output config
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{ // regra para babel transpilar
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, // regra para scss
-        {
-            test: /\.s?css$/,
-            use: ['style-loader', 'css-loader', 'sass-loader']
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+if (process.env.NODE_ENV == 'test') {
+	require('dotenv').config({ path: '.env.test' });
+} else if (process.env.NODE_ENV == 'development') {
+	require('dotenv').config({ path: '.env.development' });
+}
+
+module.exports = (env) => {
+    const isProduction = env == 'production';
+    const CSSExtract = new MiniCssExtractPlugin({ filename: 'styles.css' });
+    
+    return {
+        entry: './src/app.js',
+        // bundle output config
+        output: {
+            path: path.join(__dirname, 'public','dist'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{ // regra para babel transpilar
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, // regra para scss
+            {
+                test: /\.s?css$/,
+                use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
         }]
-    },
-    // debuger config
-    devtool: 'cheap-module-eval-source-map',
-    // local server config
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    }
-};
+        },
+        // debuger config
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        // local server config
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true,
+            publicPath: '/dist/'
+        },
+        // css extract plugin config
+        plugins: [
+            CSSExtract,
+            new webpack.DefinePlugin({
+                'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+                'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+                'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+                'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+                'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+                'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+                'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID)
+            })
+        ]
+    };
+}
+
+
+
 

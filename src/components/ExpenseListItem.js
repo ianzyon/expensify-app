f14de97e6d1246de44e98810c6efcc56ef8e40dd
ianzyon <iansalgado@yahoo.com.br>
2019-11-06 14:07:12 -0300

    display expense with numeral and moment added

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,6 +1,8 @@
 import React from 'react';
 // importar uma action generator de REMOVE
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 // stateless funcitional component
 // description, amount, createdAt
@@ -9,7 +11,7 @@ const ExpenseListItem = ({id,description, amount, createdAt}) => (
         <Link to={`/edit/${id}`}>
             <h3>{description}</h3>
         </Link>
-        <p>Amount: {amount} - Created at: {createdAt}</p>
+        <p>Amount: {numeral(amount / 100).format('$0,0.00')} - Created at: {moment(createdAt).format('MM Do, YYYY')}</p>
         
     </div>
 )
